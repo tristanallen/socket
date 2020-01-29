81f63e1b0236802bacd7a5a2cb54f28f5e1e7c94
Tristan Allen <tallen@golin.com>
2020-01-29 14:46:39 +0000

    remove start script from build

--- src/server.ts
+++ src/server.ts
@@ -43,5 +43,5 @@ wss.on('connection', (ws: WebSocket) => {
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port 8889 :)`);
+    console.log(`Server started on port ${server.address().port} :)`);
 });
